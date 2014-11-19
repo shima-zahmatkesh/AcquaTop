7e4d91a991047cf6ddf1c9350ce76431552313da
soheila dehghan <soheila.dehghanzadeh@deri.org>
2014-11-19 20:31:46 +0000

    moving DWJoinOperator to approximateJoinOperator

--- src/main/java/queryProcessor.java
+++ src/main/java/queryProcessor.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 
 
@@ -49,34 +50,37 @@ public class queryProcessor {
 		protected HashMap<Long, Integer> replica;
 		protected int updateBudget;
 
-		public void process(long timestamp){
+		public void process(long timeStamp){
+			
+			FileWriter J=new FileWriter(new File("D:/softwareData/git-clone-https---soheilade-bitbucket.org-soheilade-acqua.git/acquaProj/joinOutput/"+this.getClass().toString()+"Output.txt"));
+
+			//invoke FollowerTable::getFollowers(user,ts) and updates the replica
 			for(long id : updatePolicy()){
-				//invoke FollowerTable::getFollowers(user,ts) and updates the replica
+				replica.put(id,tfc.getUserFollowerFromDB(timeStamp, id));
 			}
 			//process the join
+			
+			long windowDiff = timeStamp-start;
+			int index=((int)windowDiff)/windowSize;			
+			HashMap<Long,Integer> mentionList = tsc.windows.get(index);
+			//we join mentionList with initial cache and return result
+			Iterator it= mentionList.keySet().iterator();
+			while(it.hasNext()){
+				long userId=Long.parseLong(it.next().toString());
+				Integer userFollowers = replica.get(userId);
+				J.write(userId +" "+mentionList.get(userId)+" "+userFollowers+"\n");
+			}
+			
 		}
-		protected abstract Set<long> updatePolicy();
+		protected abstract HashSet<Long> updatePolicy();
 	}
 	//----------------------------------------------------------------------------------------
-		public class DWJoinOperator implements ApproximateJoinOperator{
+		public class DWJoinOperator extends ApproximateJoinOperator{
 			
-			public void process(long timeStamp){
-				
-				FileWriter DWJ=new FileWriter(new File("D:/softwareData/git-clone-https---soheilade-bitbucket.org-soheilade-acqua.git/acquaProj/joinOutput/DWJoinOutput.txt"));
-				
-				long windowDiff = timeStamp-start;
-				int index=((int)windowDiff)/windowSize;			
-				HashMap<Long,Integer> mentionList = tsc.windows.get(index);
-				//we join mentionList with initial cache and return result
-				Iterator it= mentionList.keySet().iterator();
-				while(it.hasNext()){
-					long userId=Long.parseLong(it.next().toString());
-					Integer userFollowers = replica.get(userId);
-					DWJ.write(userId +" "+mentionList.get(userId)+" "+userFollowers+"\n");
-				}
-				
+			
+			protected HashSet<Long> updatePolicy(){
+				return new HashSet<Long>();
 			}
-			protected abstract Set<long> updatePolicy();
 		}
 		//----------------------------------------------------------------------------------------
 	public class BaselineJoinOperator extends ApproximateJoinOperator{
