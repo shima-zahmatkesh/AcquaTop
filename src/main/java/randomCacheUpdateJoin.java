3f6aca98873133bbb56d7d01a00f5154a81d70ff
soheila dehghan <soheila.dehghanzadeh@deri.org>
2014-11-26 15:45:36 +0000

    randomPolicyUpdated

--- src/main/java/randomCacheUpdateJoin.java
+++ src/main/java/randomCacheUpdateJoin.java
@@ -17,21 +17,24 @@ public randomCacheUpdateJoin(int ub) {
 protected HashSet<Long> updatePolicy(Iterator<Long> candidateUserSetIterator){
 	//decide which rows to update and return the list
 	//it must satisfy the updateBudget constraint!
+	//A is the list of all userids avaiable in stream
 	ArrayList<Long> A=new ArrayList<Long>();
 	while(candidateUserSetIterator.hasNext()){
 		A.add(candidateUserSetIterator.next());
 	}
-	Random rand = new Random();
-	Set<Integer> indexes = new HashSet<Integer>();
+	Random rand = new Random(System.currentTimeMillis());
+	ArrayList<Integer> indexes = new ArrayList<Integer>();
 	while(indexes.size()<updateBudget){
 	indexes.add(rand.nextInt(A.size()));
 	}
 		HashSet<Long> result=new HashSet<Long>();
 		int counter=0;
 		while(counter<updateBudget){
-			result.add(A.get(counter));
+			System.out.println("user Id: "+A.get(indexes.get(counter)));
+			result.add(A.get(indexes.get(counter)));
 			counter++;
 		}
+		System.out.println("-----------------------------------------------------------------");
 		return result;
 	}
 }
