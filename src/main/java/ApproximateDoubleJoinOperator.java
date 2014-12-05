73a43aa967d20a8517bf7d3dd1b51827c4df2768
soheila dehghan <soheila.dehghanzadeh@deri.org>
2014-12-05 20:37:36 +0000

    double join

--- src/main/java/ApproximateDoubleJoinOperator.java
+++ src/main/java/ApproximateDoubleJoinOperator.java
@@ -4,9 +4,15 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
+
+import acqua.config.Config;
+import acqua.data.TwitterFollowerCollector;
+import acqua.query.QueryProcessor;
+import acqua.query.join.JoinOperator;
 
 
-public class ApproximateDoubleJoinOperator {
+public abstract class ApproximateDoubleJoinOperator implements JoinOperator {
 	protected HashMap<Long, Integer> followerReplica;
 	protected HashMap<Long,Integer>  statusCountReplica;
 	protected HashMap<Long,Long> FollowerUpdateTime;
@@ -21,11 +27,11 @@ public class ApproximateDoubleJoinOperator {
 		statusCountReplica = TwitterFollowerCollector.getInitialUserStatusCountFromDB();
 		Iterator it = followerReplica.keySet().iterator();
 		while(it.hasNext()){
-			FollowerUpdateTime.put(Long.parseLong(it.next().toString()),queryProcessor.start);//follower info is according to the end of first window
+			FollowerUpdateTime.put(Long.parseLong(it.next().toString()),Config.INSTANCE.getQueryStartingTime());//follower info is according to the end of first window
 		}
 		it=statusCountReplica.keySet().iterator();
 		while(it.hasNext()){
-			StatusCountUpdateTime.put(Long.parseLong(it.next().toString()),queryProcessor.start);//follower info is according to the end of first window
+			StatusCountUpdateTime.put(Long.parseLong(it.next().toString()),Config.INSTANCE.getQueryStartingTime());//follower info is according to the end of first window
 		}
 		try{
 		J = new FileWriter(new File("D:/softwareData/git-clone-https---soheilade-bitbucket.org-soheilade-acqua.git/acquaProj/joinOutput/"+this.getClass().getSimpleName()+"Output.txt"));
@@ -34,21 +40,31 @@ public class ApproximateDoubleJoinOperator {
 	public void process(long timeStamp,HashMap<Long,Integer> mentionList){			
 		try {
 		//process the join			
-		long windowDiff = timeStamp-queryProcessor.start;
+		long windowDiff = timeStamp-Config.INSTANCE.getQueryStartingTime();
 		if (windowDiff==0) return;
-		int index=((int)windowDiff)/(queryProcessor.windowSize*1000);			
+		int index=((int)windowDiff)/(Config.INSTANCE.getQueryWindowWidth()*1000);			
 		//HashMap<Long,Integer> mentionList = tsc.windows.get(index);		
 		//invoke FollowerTable::getFollowers(user,ts) and updates the replica for a subset of users that exist in stream
-		for(long id : updatePolicyFollower(mentionList.keySet().iterator())){
-			followerReplica.put(id,TwitterFollowerCollector.getUserFollowerFromDB(timeStamp, id));
-			userInfoUpdateTime.put(id, timeStamp);
-		}
+		HashMap<Long,Integer> updated=updatePolicyFollowerStatusCount(mentionList.keySet().iterator());
+		Iterator<Long> it=updated.keySet().iterator();
+		while(it.hasNext()){
+			long tempUpdateUserId=it.next();
+			if(updated.get(tempUpdateUserId)==1){
+				followerReplica.put(tempUpdateUserId,TwitterFollowerCollector.getUserFollowerFromDB(timeStamp, tempUpdateUserId));
+				FollowerUpdateTime.put(tempUpdateUserId, timeStamp);
+			}
+			if(updated.get(tempUpdateUserId)==2){
+				statusCountReplica.put(tempUpdateUserId,TwitterFollowerCollector.getUserFollowerFromDB(timeStamp, tempUpdateUserId));
+				StatusCountUpdateTime.put(tempUpdateUserId, timeStamp);
+			}
+		}		
 		//we join mentionList with current replica and return result	
-		Iterator it= mentionList.keySet().iterator();
+		it= mentionList.keySet().iterator();
 		while(it.hasNext()){
 			long userId=Long.parseLong(it.next().toString());
 			Integer userFollowers = followerReplica.get(userId);
-			J.write(userId +" "+mentionList.get(userId)+" "+userFollowers+" "+timeStamp+"\n");
+			Integer StatusCount = statusCountReplica.get(userId);
+			J.write(userId +" "+mentionList.get(userId)+" "+userFollowers+" "+StatusCount+" "+timeStamp+"\n");
 		}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -56,6 +72,6 @@ public class ApproximateDoubleJoinOperator {
 		}
 	}
 	public void close(){try{J.flush();J.close();}catch(Exception e){e.printStackTrace();}}
-	public  HashSet<Long> updatePolicyFollower(Iterator<Long> candidateUserSetIterator){}
-	public HashSet<Long> updatePolicyStatusCount(Iterator<Long> candidateUserSetIterator){}
+	protected abstract  HashMap<Long,Integer> updatePolicyFollowerStatusCount(Iterator<Long> candidateUserSetIterator);
+	
 }
--- src/main/java/doublebkgJoinOperator.java
+++ src/main/java/doublebkgJoinOperator.java
@@ -1,15 +1,89 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import acqua.query.join.ApproximateJoinOperator;
+import acqua.query.join.JoinOperator;
 
 
-public class doublebkgJoinOperator extends ApproximateJoinOperator {
+public class doublebkgJoinOperator extends ApproximateDoubleJoinOperator  {
 	protected int updateBudget;
-	doublebkgJoinOperator(int ub){
+	protected HashMap<Long,Float> StatusChangeRate; 
+	protected HashMap<Long,Float> FollowerChangeRate;
+	public doublebkgJoinOperator(int ub){
 		updateBudget=ub;
+		StatusChangeRate=new HashMap<Long, Float>();
+		Connection c = null;
+	    Statement stmt = null;
+	    try {
+	      Class.forName("org.sqlite.JDBC");
+	      c = DriverManager.getConnection("jdbc:sqlite:test.db");
+	      c.setAutoCommit(false);
+	      stmt = c.createStatement();
+	      String sql="SELECT userid, ChangeRate from  ";
+	      sql="SELECT userid, changeRate from StatusCR ";
+	      //CREATE TABLE FollowerLowChr AS SELECT copyBK.uSERID as userid, CAST(CAST(count(distinct copyBK.FollowerCut) as float)/30 AS FLOAT) as changeRate from copyBK group by copyBK.uSERID
+	      System.out.println(sql);
+	      ResultSet rs = stmt.executeQuery( sql);
+	      
+	      while ( rs.next() ) {
+	         long userId = rs.getLong("userid");
+	         float userChangeRate  = rs.getFloat("changeRate");
+	         StatusChangeRate.put(userId, userChangeRate);
+	      }
+	      rs.close();
+	      sql="SELECT userid, changeRate from FollowerLowChr ";
+	      //CREATE TABLE FollowerLowChr AS SELECT copyBK.uSERID as userid, CAST(CAST(count(distinct copyBK.FollowerCut) as float)/30 AS FLOAT) as changeRate from copyBK group by copyBK.uSERID
+	      System.out.println(sql);
+	      ResultSet rs2 = stmt.executeQuery( sql);
+	      
+	      while ( rs2.next() ) {
+	         long userId = rs2.getLong("userid");
+	         float userChangeRate  = rs2.getFloat("changeRate");
+	         FollowerChangeRate.put(userId, userChangeRate);
+	      }
+	      rs2.close();
+	      stmt.close();
+	      c.close();
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	      System.exit(0);
+	    }
 	}
-		protected HashSet<Long> updatePolicy(Iterator<Long> candidateUserSetIterator){
-			HashSet<Long> result=new HashSet<Long>();		
-			
-			return result;
-		}
+		//it return the list of users with 1 which shows that user from join counterpart 1 should be updated or 2 which shows that user from join counterpart2 should be updated 
+		protected HashMap<Long, Integer> updatePolicyFollowerStatusCount(
+				Iterator<Long> candidateUserSetIterator) {
+			final class User{
+				long userId;
+				float statusExpirationTime;
+				float followerExpirationTime;
+				public User(long id,float set,float fet){userId=id;statusExpirationTime=set;followerExpirationTime=fet;}
+			}
+			List<User> userExpirationTime=new ArrayList<User>();
+			while(candidateUserSetIterator.hasNext()){
+				long userid=Long.parseLong(candidateUserSetIterator.next().toString());
+				float f= Float.parseFloat(StatusChangeRate.get(userid).toString());
+				long latestUpdateTime = Long.parseLong(StatusCountUpdateTime.get(userid).toString());
+				long statusExpectedExpirationTime=latestUpdateTime+(long)(60000/f);
+				long followerExpectedExpirationTime=latestUpdateTime+(long)(60000/f);
+				//System.out.println(expectedExpirationTime);
+				userExpirationTime.add(new User(userid, statusExpectedExpirationTime,followerExpectedExpirationTime));				
+			}
+			Collections.sort(userExpirationTime, new Comparator<User>() {
+		        public int compare(User o1, User o2) {
+		            return (int)(o1.ExpirationTime - o2.ExpirationTime);
+		        }
+		    });
+			return null;
+		}		
+		
 }
