3e5c144f1692d328866c2048e0f6a61f1570c2a1
dellaglio <dani.dellaglio@gmail.com>
2015-01-05 17:15:20 +0100

    Now VerifiedUserCollector generates the users to be used in the next steps

---
+++ src/main/java/acqua/data/MentionedUsers.java
@@ -0,0 +1,25 @@
+package acqua.data;
+
+public class MentionedUsers{
+	long[] ids;
+	String[] names;
+	int index = 0;
+	
+	public MentionedUsers(int n){
+		ids = new long[n];
+		names = new String[n];
+	}
+	
+	public void addMentionedUser(long id, String user){
+		ids[index] = id;
+		names[index++] = user;
+	}
+	
+	public long getMentionedUserId(int index){
+		return ids[index];
+	}
+	
+	public String getMentionedUserName(int index){
+		return names[index];
+	}
+}
