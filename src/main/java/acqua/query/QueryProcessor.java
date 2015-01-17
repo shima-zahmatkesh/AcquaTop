b77af0806f1d004b27cf549f8510143095e23cb1
soheila dehghan <soheila.dehghanzadeh@deri.org>
2015-01-17 22:46:09 +0000

    usersetwithdiversechangerate and newsort function based on Windows Expired

--- src/main/java/acqua/query/QueryProcessor.java
+++ src/main/java/acqua/query/QueryProcessor.java
@@ -66,7 +66,7 @@ public class QueryProcessor {
 	public static void main(String[] args){
 		QueryProcessor qp=new QueryProcessor();	
 //		qp.evaluateQuery(5);
-		for(int i=5;i<6;i++){
+		for(int i=1;i<6;i++){
 			System.out.println(i);
 			qp.evaluateQuery(i);
 		}
