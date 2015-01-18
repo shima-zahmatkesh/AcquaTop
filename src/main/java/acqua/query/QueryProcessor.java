8d4943926e4556b0d285f82a02a8a1f7115d4f8f
dellaglio <dani.dellaglio@gmail.com>
2015-01-18 22:47:07 +0100

    fixed problem with the evaluation of te

--- src/main/java/acqua/query/QueryProcessor.java
+++ src/main/java/acqua/query/QueryProcessor.java
@@ -72,11 +72,11 @@ public class QueryProcessor {
 	
 	public static void main(String[] args){
 		QueryProcessor qp=new QueryProcessor();	
-//		qp.evaluateQuery(5);
-		for(int i=5;i<7;i++){
-			System.out.println(i);
-			qp.evaluateQuery(i);
-		}
+		qp.evaluateQuery(5);
+//		for(int i=5;i<7;i++){
+//			System.out.println(i);
+//			qp.evaluateQuery(i);
+//		}
 		
 	}
 }
